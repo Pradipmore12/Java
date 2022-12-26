a9d77d27452cd5988ad0947b47a2d870da8b082f
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:05:01 +0530

    SumAB

--- SumAB.java
+++ SumAB.java
@@ -1,14 +1,14 @@
 import java.util.*;
-public class SumAB{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter two numbers");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        sc.close();
-        int sum=a+b;
-        System.out.println("sum of two numbers is: "+ sum);
 
+public class SumAB {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        int sum = a + b;
+        System.out.println("sum of two numbers is: " + sum);
 
     }
 }
