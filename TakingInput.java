53d1c95301108788e6d71688b9b3d45e64eaa472
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:09:07 +0530

    InputOutput

--- TakingInput.java
+++ TakingInput.java
@@ -1,11 +1,12 @@
 import java.util.*;
+
 public class TakingInput {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Enter your name");
-        Scanner sc= new Scanner(System.in);
-        String name=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
         sc.close();
         System.out.println("Your name  is:" + name);
     }
-    
+
 }
