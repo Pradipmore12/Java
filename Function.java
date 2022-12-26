2f04b2b1d0d13824f3f0d6dbeaa8d38f3fd671b3
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:07:06 +0530

    Functions

--- Function.java
+++ Function.java
@@ -1,19 +1,21 @@
 import java.util.*;
+
 public class Function {
-    public static int Sum(int a,int b){
-        return a+b;
+    public static int Sum(int a, int b) {
+        return a + b;
 
     }
-    public static void main(String args[]){
-        //addition of two Numbers
-        //take input
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String args[]) {
+        // addition of two Numbers
+        // take input
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Two numbers:");
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
         sc.close();
-        System.out.println(Sum(a,b));
+        System.out.println(Sum(a, b));
     }
-    
+
 }
