66bc3b2996d1ef65cd27d8c88cc6885a1d3ebb71
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:09:59 +0530

    LargestOfThree

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -1,31 +1,29 @@
 import java.util.*;
+
 public class LargestOfThree {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter three numbers");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         sc.close();
 
-        //logic starts from here
+        // logic starts from here
 
-        if(a>b){
-            if(a>c){
-                System.out.println("greater number is:"+ a);
+        if (a > b) {
+            if (a > c) {
+                System.out.println("greater number is:" + a);
+            } else {
+                System.out.println("greater number is:" + c);
             }
-            else{
-                System.out.println("greater number is:"+ c);
-            }
-        }
-        else if(b>c){
-                
-            System.out.println("greater number is:"+ b);
-        }
-        else{
-            System.out.println("greater number is:"+ c);
+        } else if (b > c) {
+
+            System.out.println("greater number is:" + b);
+        } else {
+            System.out.println("greater number is:" + c);
         }
 
     }
-    
+
 }
