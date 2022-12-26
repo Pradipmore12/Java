03b81482ec3cd1d205a9332265e8516c7281fa39
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:06:06 +0530

    StarPattern

--- StarPatternSimple.java
+++ StarPatternSimple.java
@@ -1,17 +1,18 @@
 import java.util.*;
+
 public class StarPatternSimple {
-    public static  void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Enter the number of rows and columns");
-        Scanner sc=new Scanner(System.in);
-        int rows=sc.nextInt();
-        int cols=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
         sc.close();
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=cols;j++){
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
-    
+
 }
