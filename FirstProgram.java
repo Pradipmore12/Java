13f86da21129173a19c8218cec852e54f53873cd
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:09:28 +0530

    First

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,6 @@
 //import java.util.*;
-public class FirstProgram{
-    public static void main(String args[]){
+public class FirstProgram {
+    public static void main(String args[]) {
         System.out.println("hello Pradip");
 
     }
