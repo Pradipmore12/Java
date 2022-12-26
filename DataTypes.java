e3408d59a643611694b00562fef663e362e006b2
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-26 20:07:50 +0530

    dataTypes

--- DataTypes.java
+++ DataTypes.java
@@ -1,13 +1,12 @@
 public class DataTypes {
-    public static void main(String args[]){
-        int a=20;
-        String p="Pradip";
-        char b='A';
-        float num=3.0f;
-        byte num1=2;
-        double num3=288.23;
-        System.out.println(a+p+b+num+num1+num3);
-       
+    public static void main(String args[]) {
+        int a = 20;
+        String p = "Pradip";
+        char b = 'A';
+        float num = 3.0f;
+        byte num1 = 2;
+        double num3 = 288.23;
+        System.out.println(a + p + b + num + num1 + num3);
 
-    } 
+    }
 }
