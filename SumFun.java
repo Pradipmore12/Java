8bf4bf5bf81d9445b8f3164d83c246b95e63c776
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-27 20:27:05 +0530

    SumFunction

--- SumFun.java
+++ SumFun.java
@@ -1,16 +1,18 @@
 import java.util.*;
+
 public class SumFun {
-    public static int printSum(int num1, int num2){
-        int sum=num1+num2;
+    public static int printSum(int num1, int num2) {
+        int sum = num1 + num2;
         return sum;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int num1=sc.nextInt();
-        int num2=sc.nextInt();
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
         sc.close();
-        System.out.println("Sum of "+num1+" and "+num2+" is :"+printSum(num1,num2));
+        System.out.println("Sum of " + num1 + " and " + num2 + " is :" + printSum(num1, num2));
 
     }
-    
+
 }
