9f3a522e3f5b8994fb5998e85c8e0f02db566b0f
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-27 20:25:20 +0530

    PrimeFun

--- Primefun.java
+++ Primefun.java
@@ -1,23 +1,24 @@
 import java.util.*;
+
 public class Primefun {
-    public static boolean prime(int num){
-        for(int i=2;i<num;i++){
-            if(num%i==0){
+    public static boolean prime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int  num=sc.nextInt();
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         sc.close();
-        if(prime(num)){
+        if (prime(num)) {
             System.out.println("Its a prime number");
-        }
-        else{
+        } else {
             System.out.println("Its not a prime number");
         }
     }
-    
+
 }
