4963e8e201fc1521d9c6cd0c2e0bd0559d18b85d
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-27 20:25:50 +0530

    NcRFun

--- nCrFun.java
+++ nCrFun.java
@@ -1,21 +1,23 @@
 import java.util.*;
+
 public class nCrFun {
 
-    public static int fact(int num){
-        int fact=1;
-        for(int i=2;i<=num;i++){
-            fact*=i;
+    public static int fact(int num) {
+        int fact = 1;
+        for (int i = 2; i <= num; i++) {
+            fact *= i;
         }
         return fact;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the N and C");
-        int n=sc.nextInt();
-        int r=sc.nextInt();
+        int n = sc.nextInt();
+        int r = sc.nextInt();
         sc.close();
-        int ans=fact(n)/(fact(n-r)*fact(r));
+        int ans = fact(n) / (fact(n - r) * fact(r));
         System.out.println(ans);
     }
-    
+
 }
