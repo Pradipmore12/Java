13c0488fae91edd630907e2f9cd4d7069d1c4ef7
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-27 20:28:13 +0530

    CharPattern

--- CahracterPattern.java
+++ CahracterPattern.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 public class CahracterPattern {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         sc.close();
 
-        //Code Logic Starts here
-        
-        char toPrint='A';
-        for(int row=1;row<=num;row++){
-            for(int col=1;col<=row;col++){
-                System.out.print(toPrint+" ");
+        // Code Logic Starts here
+
+        char toPrint = 'A';
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(toPrint + " ");
                 toPrint++;
             }
             System.out.println();
