f78abd03ffa7db58f053c181f6987eb0492ad8ef
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-28 21:15:27 +0530

    HollowPattern

--- Patterns/hollowPattern.java
+++ Patterns/hollowPattern.java
@@ -1,19 +1,19 @@
 import java.util.*;
+
 public class hollowPattern {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the nuber of rows and cols:");
-        int rows=sc.nextInt();
-        int cols=sc.nextInt();
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
         sc.close();
 
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=cols;j++){
-                if(j==cols || j==1 || i==1 || i==rows){
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (j == cols || j == 1 || i == 1 || i == rows) {
                     System.out.print("* ");
 
-                }
-                else{
+                } else {
                     System.out.print("  ");
                 }
             }
