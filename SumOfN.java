4344c454846bdd4e1c9acff9d8595bf869746b08
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-28 20:16:20 +0530

    SumOfN

--- SumOfN.java
+++ SumOfN.java
@@ -1,14 +1,15 @@
 import java.util.*;
+
 public class SumOfN {
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the number:");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
         sc.close();
-        int sum=0;
-        for(int i=1;i<=num;i++){
-            sum+=i;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
         }
-        System.out.println("Sum of First "+num+"numbers is :"+sum);
+        System.out.println("Sum of First " + num + "numbers is :" + sum);
     }
 }
