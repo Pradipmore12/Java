aaa26b48cc31cd965682bbee54a520ba679e9dca
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-28 20:14:47 +0530

    HalfPyramid

--- HalfPyramidPattern.java
+++ HalfPyramidPattern.java
@@ -1,15 +1,16 @@
 import java.util.*;
+
 public class HalfPyramidPattern {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         sc.close();
-        for(int row=1;row<=num;row++){
-            for(int col=1;col<=row;col++){
-                System.out.print(col+" ");
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
             }
             System.out.println();
         }
     }
-    
+
 }
