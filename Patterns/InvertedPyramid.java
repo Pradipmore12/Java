5a4cb84f37703e381b12390577380e9a923889f8
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-28 21:16:06 +0530

    InvertedPattern

--- Patterns/InvertedPyramid.java
+++ Patterns/InvertedPyramid.java
@@ -1,20 +1,21 @@
 import java.util.*;
+
 public class InvertedPyramid {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows and cols:");
-        int rows=sc.nextInt();
-        int cols=sc.nextInt();
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=rows-i;j++){
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
                 System.out.print("  ");
             }
-            for(int j=1;j<=i;j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
 
     }
-    
+
 }
