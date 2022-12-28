965417208d79e007c30d425acc2bb3cc38f2bbe5
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-28 20:15:53 +0530

    StarPattern1

--- Pattern1.java
+++ Pattern1.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 public class Pattern1 {
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        int num=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
         sc.close();
-        for(int row=1;row<=num;row++){
-            for(int col=1;col<=row;col++){
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
 
             }
             System.out.println();
         }
     }
-    
+
 }
