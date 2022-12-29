64a65aa5fdc65955ecadec817e990abdee383ee1
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-29 20:46:48 +0530

    FLoyedsTraingle

---
+++ Patterns/Rhombus.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Rhombus {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= rows; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
