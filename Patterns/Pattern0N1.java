1c71f120d302b3cf5a17c4c1129c18c72e44e27b
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-29 20:52:53 +0530

    PatternStartJava

--- Patterns/Pattern0N1.java
+++ Patterns/Pattern0N1.java
@@ -4,6 +4,7 @@ public class Pattern0N1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
+        sc.close();
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
