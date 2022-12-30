f062e2f1f68b3b76da5a26529103911eec0c7d89
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-30 20:37:36 +0530

    BinSearch

--- Arrays/BinSearch.java
+++ Arrays/BinSearch.java
@@ -1,35 +1,34 @@
 import java.util.*;
+
 public class BinSearch {
-    public static int binSearch(int arr[],int key) {
-        int s=0;
-        int e=arr.length-1;
+    public static int binSearch(int arr[], int key) {
+        int s = 0;
+        int e = arr.length - 1;
         int mid;
-        while(s<=e){
-            mid=(s+e)/2;
-            if(arr[mid]==key){
+        while (s <= e) {
+            mid = (s + e) / 2;
+            if (arr[mid] == key) {
                 return mid;
-            }
-            else if(arr[mid]>key){
+            } else if (arr[mid] > key) {
                 e--;
-            }
-            else{
+            } else {
                 s++;
             }
 
         }
         return -1;
     }
-    public static void main(String args[]){
-        int arr[]={1,2,3,4,5,6,7};
-        int key=4;
 
-        if(binSearch(arr,key)==-1){
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int key = 4;
+
+        if (binSearch(arr, key) == -1) {
             System.out.println("element not found");
-        }
-        else{
-            System.out.println("element found at index "+ binSearch(arr,key));
+        } else {
+            System.out.println("element found at index " + binSearch(arr, key));
 
         }
     }
-    
+
 }
