634dae1e0dbfc91e7a809b79bc5a78b883770e02
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-30 20:30:51 +0530

    LinSearch

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,33 +1,32 @@
 import java.util.*;
 
-import javax.sound.midi.SysexMessage;
 public class LinearSearch {
-    public static int linSearch(int nums[],int key) {
-                for(int i=0;i<nums.length;i++) {
-                    if(nums[i]==key){
-                        return i;
-                    }
-                }
-                return -1;
+    public static int linSearch(int nums[], int key) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == key) {
+                return i;
+            }
+        }
+        return -1;
     }
-    public static void main(String args[]){
-        Scanner sc =new Scanner(System.in);
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array size");
-        int size=sc.nextInt();
-        int nums[]=new int[size];
-        for(int i=0;i<nums.length;i++) {
-            nums[i]=sc.nextInt();
+        int size = sc.nextInt();
+        int nums[] = new int[size];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
         }
         System.out.println("Enter the key to find");
-        int key=sc.nextInt();
-        if(linSearch(nums,key)==-1) {
+        int key = sc.nextInt();
+        if (linSearch(nums, key) == -1) {
             System.out.println(key + " is not present");
-        }
-        else{
-            System.out.println(key+" is found at index :"+ linSearch(nums,key));
+        } else {
+            System.out.println(key + " is found at index :" + linSearch(nums, key));
 
         }
 
     }
-    
+
 }
