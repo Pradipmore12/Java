2eff0b2704960fd15c9839b2d2a7d86445ca8def
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-30 21:23:06 +0530

    SubArray

---
+++ Arrays/subArrays.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class subArrays {
+    public static void printSubarr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            for(int j=i;j<arr.length;j++){
+                for(int k=i;k<=j;k++) {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5,6};
+        printSubarr(arr);
+    }
+    
+}
