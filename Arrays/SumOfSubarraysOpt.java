e75f5d726e7ea9b784e5d296ad292386a74f9e49
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-31 20:39:59 +0530

    SumOfSubArray

--- Arrays/SumOfSubarraysOpt.java
+++ Arrays/SumOfSubarraysOpt.java
@@ -1,13 +1,16 @@
 public class SumOfSubarraysOpt {
     public static void printSumOfSubarr(int arr[]) {
+       
+        int prefix[]=new int[arr.length];
+        prefix[0]=arr[0];
+        for(int i=1;i<prefix.length;i++) {
+            prefix[i]=prefix[i-1]+arr[i];
+        }
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    System.out.print(arr[k] + " ");
-                    sum += arr[k];
-                }
+                int sum = i==0 ? prefix[j] : prefix[j] - prefix[i-1];
+                
                 System.out.println("sum is :" + sum);
                 if (sum > maxSum) {
                     maxSum = sum;
