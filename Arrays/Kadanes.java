411975945552d65cd80f8fd8028f943f1f080f96
Pradipmore12 <en19320803@git-india.edu.in>
2022-12-31 21:18:17 +0530

    SumOfSubArray

---
+++ Arrays/Kadanes.java
@@ -0,0 +1,32 @@
+public class Kadanes {
+    public static int maxSumSubarrKadanes(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum=0;
+        int max=Integer.MAX_VALUE;
+        int negCount=0;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]<0){
+                negCount++;
+            }
+            max=Math.max(arr[i]);
+        }
+        if(negCount==arr.length){
+            return max;
+        }
+        for(int i=0;i<arr.length;i++){
+            currSum+=arr[i];
+            if(currSum<0) {
+                currSum=0;
+            }
+            maxSum=Math.max(maxSum,currSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { -2,-3,4,-1,-2,1,5,-3 };
+        System.err.println(maxSumSubarrKadanes(arr)); 
+    }
+
+    
+}
