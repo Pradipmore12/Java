c2e3dfc0be13b3e4f3e360b0031da2d69286752e
Pradipmore12 <en19320803@git-india.edu.in>
2023-01-05 22:17:16 +0530

    SumFunction

--- SumFun.java
+++ SumFun.java
@@ -9,7 +9,7 @@ public class SumFun {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        int num2 =sc.nextInt();
         sc.close();
         System.out.println("Sum of " + num1 + " and " + num2 + " is :" + printSum(num1, num2));
 
