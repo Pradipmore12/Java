7e470fe962e943e8ffcb7342b4379048331e3c99
Pradipmore12 <en19320803@git-india.edu.in>
2023-01-05 22:21:47 +0530

    SimpleStarPattern

--- Patterns/StarPatternSimple.java
+++ Patterns/StarPatternSimple.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//Simple Star pattern
 public class StarPatternSimple {
     public static void main(String args[]) {
         System.out.println("Enter the number of rows and columns");
